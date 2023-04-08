aac0b495106ca65414ae3e9c8a139a1d5e10108e
Chitran1987 <ghosal.chitran@gmail.com>
2023-04-08 17:39:53 +0200

    peak window decision

--- First_investigation_kspace.R
+++ First_investigation_kspace.R
@@ -1,8 +1,22 @@
 df <- ft(X,Y, w=T)
 ClearPlot()
 ##subplots
+#First_investigation_kspace_line
 par(mfrow=c(2,2))
+plot(df$wf, abs(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlab  = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
+plot(df$wf, Arg(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlab  = 'w', ylab = 'phase', sub = 'Phase spectrum')
+plot(df$wf, abs(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlim = c(20.5,21.5), xlab = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
+plot(df$wf, Arg(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlim = c(20.5,21.5), xlab = 'w', ylab = 'phase', sub = 'Phase spectrum')
+
+#First_investigation_kspace_bubble
 plot(df$wf, abs(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlab  = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
 plot(df$wf, Arg(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlab  = 'w', ylab = 'phase', sub = 'Phase spectrum')
 plot(df$wf, abs(df$fy), type= 'b', col=rgb(0,0,1,0.25), xlim = c(20.5,21.5), xlab = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
 plot(df$wf, Arg(df$fy), type= 'b', col=rgb(0,0,1,0.25), xlim = c(20.5,21.5), xlab = 'w', ylab = 'phase', sub = 'Phase spectrum')
+
+##checking position of exact peak
+par(mfrow=c(1,1))
+ClearPlot()
+plot(df$wf, abs(df$fy), type= 'b', col=rgb(0,0,1,0.25), xlim = c(20.8,21.2), xlab = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
+abline(v=20.9, col='red')
+abline(v=21.0, col='red')
