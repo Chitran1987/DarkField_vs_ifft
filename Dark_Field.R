a2510624d6a73a7aadddefbc4a9e97cfdd20752d
Chitran1987 <ghosal.chitran@gmail.com>
2023-04-09 14:44:59 +0200

    Dark field updated with final solution

--- Dark_Field.R
+++ Dark_Field.R
@@ -1,4 +1,35 @@
 ##Divide real spectrum into bins
-n <- 4 # minimum no. of waves of this periodicity in the bin
+n <- 2 # minimum no. of waves of this periodicity in the bin
 p <- k/xsamp #no. of points available in a single unit of periodicity
-N <- ceiling(n*p) #no. of data points needed for 4units of periodicity
+N <- ceiling(n*p) #no. of data points needed for 2units of periodicity/for a single bin
+n_dark_field_bins <- floor(length(X)/N) #no. of bins created by this dark field binning over entire data set
+
+##start the dark field binning
+bin_vec <- seq(1, n_dark_field_bins) #define a bin vector
+dfld <- NULL #define a dark feild vector
+d_orig <- data.frame(X,Y)
+for (r in bin_vec) {
+  d_tmp <- d_orig[((r-1)*N+1):(r*N),]  #subset your datatframe according to the bin
+  dft_tmp_abs <- ft(d_tmp$X, d_tmp$Y, w=T)  #fourier transform it
+  dft_tmp_abs$fy <- abs(dft_tmp_abs$fy) #convert the fourier coefficients to their magnitude
+  int_val <- num_integrate(dft_tmp_abs$w, dft_tmp_abs$fy, xmin = 20, xmax = 22) #integrate between said (w values) k-vectors in k-space
+  dfld <- c(dfld, int_val)
+}
+dfld <- data.frame(bin_vec, dfld) #convert it into a data frame
+
+###recreating an envelope on the original dataset
+f_data_X <- NULL
+f_data_Y <- NULL
+for (r in bin_vec) {
+  f_data_X_tmp <- X[((r-1)*N+1):(r*N)]
+  f_data_X <- c(f_data_X, f_data_X_tmp)
+  f_data_Y_tmp <- rep(dfld$dfld[r], N)
+  f_data_Y <- c(f_data_Y, f_data_Y_tmp)
+}
+f_data_Y <- nrm(f_data)
+f_data <- data.frame(f_data_X, f_data_Y)
+
+##plotting the data
+ClearPlot()
+plot(X,Y, col=rgb(0,0,1,0.25), type = 'l')
+lines(f_data$f_data_X, f_data$f_data_Y, col='red')
