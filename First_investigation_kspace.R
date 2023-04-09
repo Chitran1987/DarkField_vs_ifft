8ef430be769908b313c52af1d68284ed369890c9
Chitran1987 <ghosal.chitran@gmail.com>
2023-04-09 11:12:48 +0200

    SOme changes done

--- First_investigation_kspace.R
+++ First_investigation_kspace.R
@@ -4,22 +4,36 @@ ClearPlot()
 #First_investigation_kspace_line
 par(mfrow=c(2,2))
 plot(df$wf, abs(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlab  = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
+abline(v=19, col='red')
+abline(v=24, col='red')
+
+# abline(h=5000, col='red')
+# abline(h=0, col='red')
+
 plot(df$wf, Arg(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlab  = 'w', ylab = 'phase', sub = 'Phase spectrum')
-plot(df$wf, abs(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlim = c(19,24), xlab = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
-plot(df$wf, Arg(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlim = c(19,24), xlab = 'w', ylab = 'phase', sub = 'Phase spectrum')
+plot(df$wf, abs(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlim = c(19,24), ylim = c(0,5000), xlab = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
+abline(v=20, col='red')
+abline(v=22, col='red')
+plot(df$wf, Arg(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlim = c(19,24), ylim=c(0,3), xlab = 'w', ylab = 'phase', sub = 'Phase spectrum')
 
 #First_investigation_kspace_bubble
+ClearPlot()
+par(mfrow=c(2,2))
 plot(df$wf, abs(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlab  = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
+abline(v=19, col='red')
+abline(v=24, col='red')
 plot(df$wf, Arg(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlab  = 'w', ylab = 'phase', sub = 'Phase spectrum')
-plot(df$wf, abs(df$fy), type= 'b', col=rgb(0,0,1,0.25), xlim = c(19,24), xlab = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
-plot(df$wf, Arg(df$fy), type= 'b', col=rgb(0,0,1,0.25), xlim = c(19,24), xlab = 'w', ylab = 'phase', sub = 'Phase spectrum')
+plot(df$wf, abs(df$fy), type= 'b', col=rgb(0,0,1,0.25), xlim = c(19,24), ylim = c(0,5000), xlab = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
+abline(v=20, col='red')
+abline(v=22, col='red')
+plot(df$wf, Arg(df$fy), type= 'b', col=rgb(0,0,1,0.25), xlim = c(19,24), ylim=c(0,3), xlab = 'w', ylab = 'phase', sub = 'Phase spectrum')
 
 ##checking position of exact peak
 par(mfrow=c(1,1))
 ClearPlot()
-plot(df$wf, abs(df$fy), type= 'b', col=rgb(0,0,1,0.25), xlim = c(19,24), xlab = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
-abline(v=19, col='red')
-abline(v=24, col='red')
+plot(df$wf, abs(df$fy), type= 'l', col=rgb(0,0,1,0.25), xlim = c(19,24), ylim=c(0,5000), xlab = 'w', ylab = 'abs', sub = 'Magnitude spectrum')
+abline(v=20, col='red')
+abline(v=22, col='red')
 
 ##Divide real spectrum into bins
 n <- 4 # minimum no. of waves of this periodicity in the bin
