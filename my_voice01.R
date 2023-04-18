b839b4ba0165a75f1ec166e290c4cd7e92689b6b
Chitran1987 <ghosal.chitran@gmail.com>
2023-04-18 02:44:00 +0200

    Generate data from my_voice_01.aup3

---
+++ my_voice01.R
@@ -0,0 +1,16 @@
+rm(list=ls())
+library(readr)
+library(tuneR)
+#path of file
+file_audio_path <- "D:/SynologyDrive/Didos_self/Career/Future/blog/LEEM_vs_inv_FFT/DarkField_vs_ifft/my_voice_01.wav"
+#Read Files
+my_audio = readWave(file_audio_path)
+#Lets see the structure of the audio.
+str(my_audio)  ## str diplays the structure of an arbitrary R object
+#check the headers of the object
+head(my_audio)
+left <- my_audio@left
+right <- my_audio@right
+t <- seq(0, (length(left)-1))*(1/(my_audio@samp.rate))
+plot(t, left, type='l', col=rgb(0,0,1,0.25))
+lines(t, left, type='l', col=rgb(0,1,0,0.25))
